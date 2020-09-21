b12b81cda41efdf877cf4d121658d20130abc89c
Ravi Stil <ravi.raaf@gmail.com>
2020-09-21 23:31:03 +0200

    Menu created

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
 // JavaScript Document
+
+var mobileOverlay = document.querySelector('#mobile-overlay');
+var mobileMenuButton = document.querySelector('#mobile-menu-button');
+var mobileCloseButton = document.querySelector('#mobile-close-button')
+
+function openMobileMenu() {
+    mobileOverlay.classList.add('active');
+} 
+
+function closeMobileMenu() {
+    mobileOverlay.classList.remove('active');
+} 
+
+mobileMenuButton.addEventListener('click', openMobileMenu);
+mobileCloseButton.addEventListener('click', closeMobileMenu);
